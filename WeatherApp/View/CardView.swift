199b65833c9d71374d4ad24291def00e2fd5cdb7
Daniel Lisovoy <danil.lisovoy@sigma.software>
2019-02-01 19:57:24 +0200

    location icon with breaking constraints

--- WeatherApp/View/CardView.swift
+++ WeatherApp/View/CardView.swift
@@ -5,6 +5,7 @@ class CardView: UIView {
   let titleLabel = UILabel()
   let subtitleLabel = UILabel()
   let imageView = UIImageView()
+  let locationIcon = UIImageView(image: #imageLiteral(resourceName: "current_location"))
   let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
   let vibrancyEffectView = UIVisualEffectView(effect: UIVibrancyEffect(blurEffect: UIBlurEffect(style: .light)))
   
@@ -23,6 +24,7 @@ class CardView: UIView {
     visualEffectView.contentView.addSubview(vibrancyEffectView)
     vibrancyEffectView.contentView.addSubview(titleLabel)
     vibrancyEffectView.contentView.addSubview(subtitleLabel)
+    vibrancyEffectView.contentView.addSubview(locationIcon)
   }
   
   private func setupLayout() {
@@ -32,7 +34,12 @@ class CardView: UIView {
     titleLabel.snp.makeConstraints { make in
       make.top.equalToSuperview().offset(8)
       make.left.equalToSuperview().offset(8)
-      make.right.equalToSuperview().offset(8)
+    }
+    locationIcon.setContentHuggingPriority(UILayoutPriority(rawValue: 500), for: .horizontal)
+    locationIcon.snp.makeConstraints { make in
+      make.centerY.equalTo(titleLabel)
+      make.left.equalTo(titleLabel.snp.right).offset(8)
+      make.right.equalToSuperview().offset(-8)
     }
     subtitleLabel.snp.makeConstraints { make in
       make.top.equalTo(titleLabel.snp.bottom).offset(8)
@@ -58,5 +65,6 @@ class CardView: UIView {
     subtitleLabel.font = UIFont.systemFont(ofSize: 12)
     
     imageView.contentMode = .scaleAspectFill
+    locationIcon.isHidden = true
   }
 }
--- WeatherApp/ViewModels/CitiesList/WeatherViewModel.swift
+++ WeatherApp/ViewModels/CitiesList/WeatherViewModel.swift
@@ -4,11 +4,13 @@ import Result
 class WeatherViewModel: BaseViewModel {
   let weather: Weather
   let photo: Property<URL?>
+  let isCurrent: Property<Bool>
   let select: Action<(), (), NoError>
   
-  init(weather: Weather, photo: URL?, select: Action<(), (), NoError>) {
+  init(weather: Weather, photo: URL?, isCurrent: Property<Bool>, select: Action<(), (), NoError>) {
     self.weather = weather
     self.photo = Property(value: photo)
+    self.isCurrent = isCurrent
     self.select = select
   }
   
