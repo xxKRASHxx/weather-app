e048e9d4cf8152ef635d7e8d3ff3000c2ca9bed6
Daniel Lisovoy <danil.lisovoy@sigma.software>
2019-02-03 19:51:55 +0200

    make constraints

--- WeatherApp/View/CardView.swift
+++ WeatherApp/View/CardView.swift
@@ -31,29 +31,30 @@ class CardView: UIView {
     imageView.snp.makeConstraints { make in
       make.edges.equalTo(self)
     }
-    titleLabel.snp.makeConstraints { make in
-      make.top.equalToSuperview().offset(8)
-      make.left.equalToSuperview().offset(8)
-    }
-    locationIcon.setContentHuggingPriority(UILayoutPriority(rawValue: 500), for: .horizontal)
-    locationIcon.snp.makeConstraints { make in
-      make.centerY.equalTo(titleLabel)
-      make.left.equalTo(titleLabel.snp.right).offset(8)
-      make.right.equalToSuperview().offset(-8)
-    }
-    subtitleLabel.snp.makeConstraints { make in
-      make.top.equalTo(titleLabel.snp.bottom).offset(8)
-      make.leading.equalTo(titleLabel.snp.leading)
-      make.right.equalToSuperview().offset(8)
-      make.bottom.equalTo(vibrancyEffectView).offset(-8)
+    vibrancyEffectView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+        .inset(UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8))
     }
     visualEffectView.snp.makeConstraints { make in
       make.top.equalToSuperview()
       make.left.equalToSuperview()
       make.right.equalToSuperview()
     }
-    vibrancyEffectView.snp.makeConstraints { make in
-      make.edges.equalToSuperview()
+    titleLabel.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.left.equalToSuperview()
+      make.right.equalTo(locationIcon.snp.left)
+    }
+    locationIcon.setContentHuggingPriority(UILayoutPriority(rawValue: 755), for: .horizontal)
+    locationIcon.snp.makeConstraints { make in
+      make.centerY.equalTo(titleLabel)
+      make.right.equalToSuperview()
+    }
+    subtitleLabel.snp.makeConstraints { make in
+      make.top.equalTo(titleLabel.snp.bottom).offset(8)
+      make.leading.equalTo(titleLabel)
+      make.right.equalToSuperview()
+      make.bottom.equalToSuperview()
     }
   }
   
